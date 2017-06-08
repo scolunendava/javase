dfb003ca694724d1a45de9837505883d9a4b96ea
dandrunakievich <dimcikxd@rambler.ru>
2017-06-08 14:32:46 +0300

    test passed

--- src/test/java/com/tutorial/SecondProblem/SecondProblemTest.java
+++ src/test/java/com/tutorial/SecondProblem/SecondProblemTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /*
- * Created by dandrunakievich on 6/8/2017.
+ Created by dandrunakievich on 6/8/2017.
  */
 public class SecondProblemTest {
 
