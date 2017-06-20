95e5b8fde35c6b97b150224b931fa6a8c83eee51
dandrunakievich <dimcikxd@rambler.ru>
2017-06-20 16:58:44 +0300

    changes

--- src/main/java/com/tutorial/sixthproblem/Main.java
+++ src/main/java/com/tutorial/sixthproblem/Main.java
@@ -3,15 +3,16 @@ package com.tutorial.sixthproblem;
 /**
  * Created by dandrunakievich on 6/20/2017.
  */
-public class Main {
-    public static void main(String[]args){
-        Run[]a=new Run[]{new Audi(),new BMW()};
 
-        for(int i=0;i<a.length;i++){
+public class Main {
+    public static void main(String[] args) {
+        Run[] a = new Run[]{new Audi(), new BMW()};
+
+        for (int i = 0; i < a.length; i++) {
             a[i].runs();
         }
 
-        Audi auto=new Audi();
+        Audi auto = new Audi();
         System.out.println(auto.runs(50));
     }
 }
--- src/main/java/com/tutorial/sixthproblem/Run.java
+++ src/main/java/com/tutorial/sixthproblem/Run.java
@@ -3,8 +3,7 @@ package com.tutorial.sixthproblem;
 /**
  * Created by dandrunakievich on 6/20/2017.
  */
+
 public interface Run {
-     void runs();
-
-
+    void runs();
 }
